accf8dd0b239f3dbe55e778e7999da2850f5e859
Chris Thorpe <drchristhorpe@googlemail.com>
2018-08-08 10:50:17 +0100

    Added in more complex logic for testing either a basic auth or api key authentication

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,7 @@ import json
 
 import constants
 import errors
+import logging
 
 
 def build_json_response(data, created=False, created_key=None, error=False):
@@ -18,10 +19,6 @@ def build_json_response(data, created=False, created_key=None, error=False):
     return response
 
 
-def check_auth(username, password):
-    return username == constants.API['basicAuth']['username'] and password == constants.API['basicAuth']['password']
-
-
 def return_error(error_condition):
     data = errors.ERRORS[error_condition]
     data['instance'] = request.path
@@ -29,11 +26,45 @@ def return_error(error_condition):
     return build_json_response(data, error=True)
 
 
+def check_auth(username, password):
+    return username == constants.API['basicAuth']['username'] and password == constants.API['basicAuth']['password']
+
+
+def check_api_key(_apikey):
+    return _apikey == constants.API['apiKey']
+
+
+def get_api_key():
+    if 'x-api-key' in request.headers:
+        return request.headers['x-api-key']
+    else:
+        return False
+
+
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return return_error('no_api_token_provided')
+        _auth = request.authorization
+        _apikey = get_api_key()
+
+        if _auth:
+            _auth_check = check_auth(_auth.username, _auth.password)
+        else:
+            _auth_check = False
+
+        if _apikey:
+            _apikey_check = check_api_key(_apikey)
+        else:
+            _apikey_check = False
+
+        if not _auth and not _apikey:
+            return return_error('unauthenticated')
+        else:
+            if _auth:
+                if not _auth_check:
+                    return return_error('invalid_authorization_details')
+            else:
+                if not _apikey_check:
+                    return return_error('invalid_api_token_provided')
         return f(*args, **kwargs)
     return decorated
