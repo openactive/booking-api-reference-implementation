e264920ef4a5f840de25bca99602eff1253e103d
Chris Thorpe <drchristhorpe@googlemail.com>
2018-08-08 10:49:46 +0100

    Added in error dictionaries for authentication errors and 404/410 errors

--- app/errors.py
+++ app/errors.py
@@ -1,11 +1,61 @@
 ERRORS = {
+    "unauthenticated": {
+        "@context": "https://openactive.io/ns/oa.jsonld",
+        "type": "Error",
+        "errorType": "https://openactive.io/errors/unauthenticated",
+        "title": "No API token or authentication provided",
+        "details": "The broker did not provide an API token or basic authentication.",
+        "status": "403",
+        "instance": "",
+        "method": ""
+    },
     "no_api_token_provided": {
         "@context": "https://openactive.io/ns/oa.jsonld",
         "type": "Error",
         "errorType": "https://openactive.io/errors/no_api_token_provided",
-        "title": "No API token or authentication provided",
-        "details": "A nice error message in here",
-        "status": "403",
+        "title": "No API token provided",
+        "details": "The broker did not provide an API token in the x-api-key request header.",
+        "status": "401",
+        "instance": "",
+        "method": ""
+    },
+    "invalid_api_token_provided": {
+        "@context": "https://openactive.io/ns/oa.jsonld",
+        "type": "Error",
+        "errorType": "https://openactive.io/errors/invalid_api_token_provided",
+        "title": "Incorrect API token provided",
+        "details": "The broker provided an invalid API token in the x-api-key request header.",
+        "status": "401",
+        "instance": "",
+        "method": ""
+    },
+    "invalid_authorization_details": {
+        "@context": "https://openactive.io/ns/oa.jsonld",
+        "type": "Error",
+        "errorType": "https://openactive.io/errors/invalid_authorization_details",
+        "title": "Incorrect basic authentication credentials provided",
+        "details": "The broker provided invalid basic authentication credentials.",
+        "status": "401",
+        "instance": "",
+        "method": ""
+    },
+    "not_found": {
+        "@context": "https://openactive.io/ns/oa.jsonld",
+        "type": "Error",
+        "errorType": "https://openactive.io/errors/not_found",
+        "title": "Not found",
+        "details": "The endpoint or resource you requested can not been found.",
+        "status": "404",
+        "instance": "",
+        "method": ""
+    },
+    "gone": {
+        "@context": "https://openactive.io/ns/oa.jsonld",
+        "type": "Error",
+        "errorType": "https://openactive.io/errors/gone",
+        "title": "Gone",
+        "details": "The endpoint or resource you requested is no longer available",
+        "status": "410",
         "instance": "",
         "method": ""
     }
