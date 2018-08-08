41bf2a2af1d2e97a4ca15b0adb79d171f0323901
Chris Thorpe <drchristhorpe@googlemail.com>
2018-08-08 10:49:12 +0100

    Added apiKey to constants

--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,8 @@
 API = {
-    "version": "0.7",
+    "version": "0.8",
     "basicAuth": {
         "username": 'openactive',
         "password": 'UsingDataToHelpPeopleGetActive'
-    }
+    },
+    "apiKey": "rr4nlnyhi3tdoonxfysi"
 }
